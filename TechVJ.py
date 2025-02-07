bb56983ea411aac991048e30d12314ba4abbbd40
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-02-07 08:35:59 +0700

    Update TechVJ.py

--- TechVJ.py
+++ TechVJ.py
@@ -1,19 +1,21 @@
-import os
-import pyrogram
+
 from pyrogram import Client
 
 api_id = 6897064
 api_hash = "206c2035a8dc342ab70421ea4094ac49"
 
 try:
-   os.remove("techvj.session")
+    os.remove("techvj.session")
 except:
-     pass
+    pass
+
 with Client("techvj", api_id=api_id, api_hash=api_hash) as app:
     session = f"**🥀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 » 𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 💞**\n\n`{app.export_session_string()}`\n\n**💥 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲: [H4RS] ✨**"
     app.send_message("me", session, disable_web_page_preview=True)
-    except:
-        pass
-    print(f"✅ String Session Has 🌟 Been Sent\nTo Your 🔥 Saved Message ✨ ...")
-    os.remove("techvj.session")
 
+print(f"✅ String Session Has 🌟 Been Sent\nTo Your 🔥 Saved Message ✨ ...")
+
+try:
+    os.remove("techvj.session")
+except:
+    pass
